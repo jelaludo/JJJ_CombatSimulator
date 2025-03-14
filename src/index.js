d6c62c6817f6b664e810ada19aad8448724b0129
jelaludo <40551265+jelaludo@users.noreply.github.com>
2025-03-14 21:21:26 +0900

    Add Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import JJJCombat from './JJJCombat';
 
@@ -7,5 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <JJJCombat />
+    <Analytics />
   </React.StrictMode>
 ); 
